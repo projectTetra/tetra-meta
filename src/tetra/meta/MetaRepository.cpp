c7d9394d2dfc02525a92741db98c093d2c3d5e9b
brad <lyman.brad3211@gmail.com>
2014-09-07 23:11:26 -0400

    created and implemented the MetaRepository

---
+++ src/tetra/meta/MetaRepository.cpp
@@ -0,0 +1,22 @@
+#include <tetra/meta/MetaRepository.hpp>
+
+using namespace std;
+using namespace tetra;
+using namespace tetra::meta;
+
+void MetaRepository::addType( const string& name,
+                              const MetaData* metaData ) noexcept
+{
+    getMetaDataMap().emplace( name, metaData );
+}
+
+const MetaData& MetaRepository::lookupMetaData( const string& name )
+{
+    return *getMetaDataMap().at( name );
+}
+
+unordered_map<string, const MetaData*>& MetaRepository::getMetaDataMap()
+{
+    static unordered_map<string, const MetaData*> metaMap;
+    return metaMap;
+}
