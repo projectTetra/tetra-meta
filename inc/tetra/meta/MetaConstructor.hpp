162595dcd8117ae932f0fa9c6b39f226d96e4c70
brad <lyman.brad3211@gmail.com>
2014-09-08 00:24:27 -0400

    created the META_REGISTER macro which will register a type with the MetaRepository so that its MetaData can be looked up by the types string name

--- inc/tetra/meta/MetaConstructor.hpp
+++ inc/tetra/meta/MetaConstructor.hpp
@@ -3,6 +3,7 @@
 #define TETRA_META_METACONSTRUCTOR_HPP
 
 #include <tetra/meta/MetaData.hpp>
+#include <tetra/meta/MetaRepository.hpp>
 
 namespace tetra
 {
@@ -16,6 +17,15 @@ template <class T>
 class MetaConstructor
 {
   public:
+    /**
+     * Register's the generated MetaData with the MetaRepository.
+     * @param name The name of the type to add to the repository
+     **/
+    MetaConstructor( const std::string& name )
+    {
+        MetaRepository::addType( name, getMetaData() );
+    }
+
     /**
      * Retrieve the MetaData associated with this type.
      * @return The MetaData for T
@@ -36,5 +46,18 @@ const MetaData& meta()
 } /* namespace meta */
 } /* namespace tetra */
 
+#define TETRA_CONCAT_IMPL( x, y ) x##y
+#define TETRA_CONCAT( x, y ) TETRA_CONCAT_IMPL( x, y )
+#define TETRA_GENERATED_NAME TETRA_CONCAT( TETRA_GENERATED, __LINE__ )
+
+/**
+ * Registers a type with the MetaRepository so that its MetaData can be looked
+ * up by name. This should only be called once per type (no harm if it is called
+ * to many times, though it could make the resulting executable larger).
+ * Basically, just stick this macro in your type's implementation file (.cpp)
+ * and you should be in good shape.
+ **/
+#define META_REGISTER( type )                                                  \
+    MetaConstructor<type> TETRA_GENERATED_NAME( #type );
 
 #endif
--- src/tetra/meta/MetaRepositoryTest.cpp
+++ src/tetra/meta/MetaRepositoryTest.cpp
@@ -9,6 +9,27 @@ using namespace std;
 using namespace tetra;
 using namespace tetra::meta;
 
+class MyType
+{
+  public:
+    MyType()
+    {
+        ++MyType::count;
+    }
+
+    ~MyType()
+    {
+        --MyType::count;
+    }
+
+  public:
+    static int count;
+};
+
+int MyType::count{0};
+
+META_REGISTER( MyType );
+
 SCENARIO( "The MetaRepository should support adding and looking up MetaData "
           "instances",
           "[MetaRepository][MetaData]" )
@@ -33,3 +54,30 @@ SCENARIO( "The MetaRepository should support adding and looking up MetaData "
         }
     }
 }
+
+SCENARIO( "The MetaRepository should support looking up registered types",
+          "[MetaRepository][MetaData][Registered]" )
+{
+    WHEN( "A type is registered with META_REGISTER" )
+    {
+        const MetaData& myTypeMeta = MetaRepository::lookupMetaData( "MyType" );
+        void* myType = myTypeMeta.construct();
+
+        THEN( "The type's constructor should be callable" )
+        {
+            REQUIRE( MyType::count == 1 );
+        }
+
+        myTypeMeta.destroy( myType );
+        THEN( "The type's destructor should be callable" )
+        {
+            REQUIRE( MyType::count == 0 );
+        }
+
+        THEN( "The MetaData should have the right size stored" )
+        {
+            REQUIRE( myTypeMeta.size() == sizeof( MyType ) );
+        }
+    }
+}
+
